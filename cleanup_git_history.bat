94f4c78bdd0725499da63bcb96f7ac6b6a17edd8
Sofonias Mengistu <69535143+Sofoniasm@users.noreply.github.com>
2025-08-11 13:11:42 +0300

    Git hygiene: ignore tfstate backups; extend cleanup script to purge tfstate from history

--- cleanup_git_history.bat
+++ cleanup_git_history.bat
@@ -2,8 +2,11 @@
 REM Download BFG Repo-Cleaner
 curl -L -o bfg.jar https://repo1.maven.org/maven2/com/madgag/bfg/1.14.0/bfg-1.14.0.jar
 
-REM Run BFG to delete .terraform folder from history
+REM Run BFG to delete .terraform folder and Terraform state files from history
 java -jar bfg.jar --delete-folders .terraform --no-blob-protection
+java -jar bfg.jar --delete-files "*.tfstate"
+java -jar bfg.jar --delete-files "*.tfstate.*"
+java -jar bfg.jar --delete-files "terraform.tfstate*"
 
 REM Clean and update repository
 git reflog expire --expire=now --all
